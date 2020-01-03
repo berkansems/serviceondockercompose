c0e1dacde058eed4447c40ecd0036b1e03449e1c
berkan <berkansems@gmail.com>
2020-01-04 00:29:47 +0300

    second-version

--- commands.sh
+++ commands.sh
@@ -1,6 +1,14 @@
-cd /etc/systemd/system
-nano docker-compose@.service
-cd /etc/docker/
-mkdir compose
-mkdir mysql
-nano docker-compose.yml
+#!/bin/bash
+#cd /etc/systemd/system
+cp ./docker-compose@.service /etc/systemd/system
+#cd /etc/docker/
+mkdir /etc/docker/compose/mysq
+#cd compose
+#mkdir mysql
+#cd mysql
+cp ./docker-compose /etc/docker/compose/mysql
+
+sudo systemctl enable docker-compose@mysql
+sudo systemctl start docker-compose@mysql
+sudo systemctl status docker-compose@mysql
+
