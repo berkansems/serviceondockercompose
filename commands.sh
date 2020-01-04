528b5a580eab3b492d6996ed555400d1bcc8bc64
berkan <berkansems@gmail.com>
2020-01-04 12:24:20 +0300

    second

--- commands.sh
+++ commands.sh
@@ -2,5 +2,13 @@ cd /etc/systemd/system
 nano docker-compose@.service
 cd /etc/docker/
 mkdir compose
+cd compose
 mkdir mysql
+cd mysql
 nano docker-compose.yml
+
+
+
+sudo systemctl enable docker-compose@mysql
+sudo systemctl start docker-compose@mysql
+sudo systemctl status docker-compose@mysql
